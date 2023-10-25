e8b87b1a30c7fa871fded1aa737ac41d6d7746cc
AutomationTesting0612 <automationtesting0612@gmail.com>
2023-10-25 21:13:29 +0530

    fixed Mongo DB issue

--- src/main/java/com/example/Cabeasebackend/Controller/EnquiryFormController.java
+++ src/main/java/com/example/Cabeasebackend/Controller/EnquiryFormController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 @Controller
 public class EnquiryFormController {
@@ -15,19 +19,35 @@ public class EnquiryFormController {
     @Autowired
     private EnquiryFormService enquiryFormService;
 
+    private Timer timer = new Timer();
+
 
 
     @GetMapping("")
     public String getIndex(Model model ){
         model.addAttribute("enquiry",new EnquiryForm());
-
         return "index";
     }
 
+    @GetMapping("/enquiry/new")
+    public String createEnquiry(Model model){
+        EnquiryForm enquiry= new EnquiryForm();
+        model.addAttribute("enquiry", enquiry);
+        return "index";
+    }
+
+
     @PostMapping("/enquiry")
-    public String newEnquiry(@ModelAttribute("enquiry") EnquiryForm enquiryForm)
+    public String newEnquiry(@ModelAttribute("enquiry") EnquiryForm enquiryForm, HttpSession session)
     {
         enquiryFormService.postEnquiry(enquiryForm);
+        session.setAttribute("message", "The enquiry has sent!");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                session.removeAttribute("message");
+            }
+        }, 5000);
         return "redirect:/";
     }
 
---
+++ src/main/resources/js/script.js
@@ -0,0 +1,7 @@
+if (confirm("Are you sure you want to proceed?")) {
+    // Code to execute if the user clicks "OK"
+    alert("You clicked OK. Proceeding...");
+} else {
+    // Code to execute if the user clicks "Cancel" or closes the dialog
+    alert("You clicked Cancel. Canceling...");
+}
